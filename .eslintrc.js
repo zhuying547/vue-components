8f22f445afe5d5444acb1eaa8bb487bb38444597
Zhu Ying <zhuying547@qq.com>
2023-07-23 10:26:03 +0800

    添加vue-cli插件

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
   },
