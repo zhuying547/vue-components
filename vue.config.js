516c3fbeab219abb5e03a27adebcaef5742342a2
Zhu Ying <zhuying547@qq.com>
2023-07-23 11:49:29 +0800

    添加githubpages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-components/' : '/',
   chainWebpack: (config) => {
     config.plugins.delete('prefetch');
   },
