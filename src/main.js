372683482f1a3689d86661483bae083545304f40
zhuying <zhuying547@qq.com>
2023-08-02 17:14:13 +0800

    添加样式集合

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 Vue.config.productionTip = false;
+
 import 'bootstrap/dist/css/bootstrap.css';
+import '@/style/index.less';
 
 new Vue({
   router,
