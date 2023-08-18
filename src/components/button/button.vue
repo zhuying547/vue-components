92cab1b951a9031f69a04b84a03f9b616f773ffa
zhuying <zhuying547@qq.com>
2023-08-18 14:26:43 +0800

    button

---
+++ src/components/button/button.vue
@@ -0,0 +1,11 @@
+<script>
+import { h } from 'vue'
+const prefixCls = 'ivu-btn'
+export default {
+  name: 'Button',
+  // 声明渲染函数
+  render() {
+    return h('button', { id: 'foo', class: 'bar' })
+  }
+}
+</script>
